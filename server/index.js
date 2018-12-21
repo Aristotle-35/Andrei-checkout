1c745d4832962b8749a3e869a3362cb6da778353
Arshavin86 <sevost.andrey@gmail.com>
2018-12-20 19:01:00 -0800

    This commit closes ticket # 4

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const {Car} = require ('../database/index.js');
+const express = require('express');
+let app = express();
+const loadNewCar = require('../faker.js');
+
+var bodyParser = require('body-parser');
+
+const jsonParser = bodyParser.json();
+
+const cors = require('cors');
+
+app.use(cors());
+app.options('*', cors());
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+let port = 3000;
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
