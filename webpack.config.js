93647b1524405f3e1a389da1f754e42ddbe40470
Arshavin86 <sevost.andrey@gmail.com>
2018-12-21 11:56:46 -0800

    set up Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,16 @@ module.exports = {
     path: DIST_DIR
   },
   module : {
-    loaders : [
+    rules: [
       {
         test : /\.jsx?/,
+        exclude: /(node_modules|bower_components)/,
         include : SRC_DIR,
-        loader : 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
+        use: {
+          loader : 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
       }
     ]
