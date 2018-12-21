335d6819a984c7cbdc613dc790bb2a402aaa4953
Arshavin86 <sevost.andrey@gmail.com>
2018-12-20 19:03:29 -0800

    Router for POST Mock data is added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const {Car} = require ('../database/index.js');
 const express = require('express');
 let app = express();
+
 const loadNewCar = require('../faker.js');
 
 var bodyParser = require('body-parser');
@@ -13,6 +14,11 @@ app.use(cors());
 app.options('*', cors());
 
 app.use(express.static(__dirname + '/../client/dist'));
+
+app.post('/cars', jsonParser, (req, res) => {
+  // console.log(req);
+  loadNewCar();
+});
 
 let port = 3000;
 
