124b440410b492da92e834b174ce22f7393cfaf0
Arshavin86 <sevost.andrey@gmail.com>
2018-12-21 12:35:18 -0800

    The main component

---
+++ client/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor () {
+    super ();
+    }
+
+  render () {
+    return (
+      <div>
+          I'm rendering now!
+      </div>
+      )
+  }
+}
+
+export default App;
