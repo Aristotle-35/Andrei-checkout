4acf0e3a1f02aa6ad90b249e30a722d5e1e13ec5
Arshavin86 <sevost.andrey@gmail.com>
2018-12-21 22:28:25 -0800

    Able to render data on localhost:3000

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
+import Price from './Price.jsx';
 
-
+var server = "http://localhost:3000/api/turash/checkouts"
 class App extends React.Component {
   constructor (props) {
     super (props);
+    this.state = {
+      price: ''
     }
+  }
+
+  getCars () {
+    fetch(`${server}/1`)
+    .then(res => {
+      if(res.ok) {
+        return res.json()
+      } else {
+        throw new Error('No result!');
+      }
+    })
+    .then(
+      (result) => {
+        console.log(result)
+        this.setState({
+          price: result[0]['price']
+        });
+        
+      })
+    .catch(error => console.log(error))
+  }
+
+  componentDidMount () {
+    this.getCars();
+  }
 
   render () {
+    console.log('price', this.state.price)
     return (
       <div>
-          I'm rendering now!
+          <Price price={this.state.price} />
       </div>
       )
   }
