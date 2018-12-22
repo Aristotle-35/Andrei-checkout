8dd70ff8ea04f793e5a9659f42c9af138bf42cb7
Arshavin86 <sevost.andrey@gmail.com>
2018-12-21 19:41:15 -0800

    Style modifications

--- faker.js
+++ faker.js
@@ -3,14 +3,14 @@ const faker = require ('faker');
 const db = require('./database/index.js');
 
 Date.prototype.addDays = function(days) {
-  var date = new Date(this.valueOf());
+  let date = new Date(this.valueOf());
   date.setDate(date.getDate() + days);
   return date;
 }
 
 const getDates = (startDate, stopDate) => {
-  var dateArray = new Array();
-  var currentDate = startDate;
+  let dateArray = new Array();
+  let currentDate = startDate;
   while (currentDate <= stopDate) {
       dateArray.push(new Date (currentDate));
       currentDate = currentDate.addDays(1);
@@ -18,25 +18,25 @@ const getDates = (startDate, stopDate) => {
   return dateArray;
 }
 
-const loadNewCar = ((times=100) => {
+const loadNewCar = ((times=101) => {
 
-  for (var i = 0; i < times; i++) {
-    let rangeTimes = faker.random.number({
+  for (let i = 1; i < times; i++) {
+    const rangeTimes = faker.random.number({
         'min': 1,
         'max': 5
         });
-      var date = new Date();
-      var date1 = faker.date.soon(30);
-      var reservedDates = [];
-    for (var j = 0; j < rangeTimes; j++) {
-      var dateStart = faker.date.between(date, date1);
-      var dateEnd = faker.date.soon(30, dateStart);
-      var range = getDates(dateStart, dateEnd);
+      let date = new Date();
+      let date1 = faker.date.soon(30);
+      let reservedDates = [];
+    for (let j = 0; j < rangeTimes; j++) {
+      let dateStart = faker.date.between(date, date1);
+      let dateEnd = faker.date.soon(30, dateStart);
+      let range = getDates(dateStart, dateEnd);
       reservedDates.push(range);
       date = dateEnd;
       date1 = faker.date.soon(30, date);
     }
-    var document = {
+    const document = {
       id: i,
       location: {carLocation: faker.fake("{{address.city}}, {{address.state}}, {{address.zipCode}}"), airport: faker.hacker.abbreviation()},
       time: { open: faker.random.number({
