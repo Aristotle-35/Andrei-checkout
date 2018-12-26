7ca399d403e3655474187e7b8a80f7f5e3a51d09
Arshavin86 <sevost.andrey@gmail.com>
2018-12-26 14:36:43 -0800

    Function to transform worldtime format to AM/PM

--- client/components/Dropdown.jsx
+++ client/components/Dropdown.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import _ from 'underscore';
+
+const timeSwitch = {
+    0: 'Midnight',
+    12: 'Noon',
+
+}
 
 class Dropdown extends React.Component {
     constructor (props) {
@@ -8,19 +15,53 @@ class Dropdown extends React.Component {
         headerTitle: this.props.title
       }
     }
-  
-    render () {
-      return (
-        <tbody>
-            <tr>
-                <td>
-                    Dropdown 
-                </td>
-            </tr>
-            
-        </tbody>
+
+    handleClickOutside() {
+        this.setState({
+          listOpen: false
+        })
+      }
+
+      toggleList() {
+        this.setState(prevState => ({
+          listOpen: !prevState.listOpen
+        }))
+      }
+
+      timeRender(time) {
+        let timeRange = _.range(time.open, time.close+1)
+        console.log(timeRange);
+        let list = _.map(timeRange, num => {
+            if (timeSwitch.hasOwnProperty(num)) {
+                num = timeSwitch[num];
+                return num;
+            } else if (num > 12) {
+                num -= 12;
+                return `${num}:00 PM`;
+            } else {
+                return `${num}: AM`;
+            }
+        })
+        console.log(list);
+        // list.map((item) => (
+        //     <li className="dd-list-item" key={item.id} >{item.title}</li>
+        //    ))
+      }
+
+      render(){
+        const{time} = this.props
+        const{listOpen, headerTitle} = this.state
+        return(
+          <div className="dd-wrapper">
+          <div className="dd-header" onClick={() => this.toggleList()}>
+              <div className="dd-header-title">{headerTitle}</div>
+          </div>
+           {listOpen && <ul className="dd-list">
+             {this.timeRender(time)}
+            </ul>}
+          </div>
         )
-    }
+      }
   }
   
   export default Dropdown;
